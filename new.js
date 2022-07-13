2ae05221f7d1bc7c3158dcd8b2abc7a0cf11e392
simegney <simegne.yifru@gmail.com>
2022-07-13 10:42:39 -0400

    initial commit

---
+++ new.js
@@ -0,0 +1,26 @@
+var day = "";
+
+switch (day){
+    case "Monday":
+        console.log(day);
+        break;
+       
+        case "Tuesday":
+        console.log(day);
+        break;
+        case "Wendnsday":
+        console.log(day);
+        break;
+        case "Thursday":
+        console.log(day);
+        break;
+        case "Friday":
+        console.log(day);
+        break;
+        case "Saturday":
+            console.log(day);
+            break;
+        default :
+        console.log("Sunday");
+        break;
+}
